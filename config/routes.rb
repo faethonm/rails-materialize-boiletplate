d96b64c8f5e712e793c46de247abfd71d97e71c9
Faethon Milikouris <faethon.milikouris@gmail.com>
2016-03-06 22:10:59 -0800

    Add devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :permutators
   root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should validate_presence_of(:email) }
+  it { should be_valid }
+
+end
