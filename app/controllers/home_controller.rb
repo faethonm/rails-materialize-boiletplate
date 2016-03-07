81ebfae2f187cfb24af2573166bbfaf0ff0a1e49
Faethon Milikouris <faethon.milikouris@gmail.com>
2016-03-06 23:28:19 -0800

    Sign in Sign out with Devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:contact]
   def index
+  end
+
+  def contact
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { FFaker::Internet.email }
+    password { FFaker::Internet.email }
   end
 end
